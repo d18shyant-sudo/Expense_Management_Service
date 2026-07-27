229b2982c6b321a68d4ca4e8b89409091584b465
Dushyant <dushyant@gmail.com>
2026-07-27 12:48:31 +0530

    role authentication implementation

--- EMS/auth.py
+++ EMS/auth.py
@@ -1,12 +1,50 @@
 import jwt
-from datetime import datetime,timedelta
-def create_access_token(username:str,password:str):
-    Secret_key ="a9f83kLz7xQp2mN5vR8sT1yU4wE6cD0h"
-    ALGORITHM ="HS256"
-    payload = {"sub":username,"role":"admin","exp":datetime.utcnow()+timedelta(minutes=30)}
-    token = jwt.encode(payload,Secret_key,algorithm=ALGORITHM)
-    return {
-            "access_token":token,
-            "token_type":"bearer",
-            "expire_at":payload["exp"]
-            }
+
+from fastapi import Depends, HTTPException
+from fastapi.security import OAuth2PasswordBearer
+
+
+oauth2_scheme = OAuth2PasswordBearer(
+    tokenUrl="/api/v1/login"
+)
+
+
+SECRET_KEY = "secret_key"
+
+
+def get_current_user(
+    token: str = Depends(oauth2_scheme)
+):
+
+    try:
+
+        payload = jwt.decode(
+            token,
+            SECRET_KEY,
+            algorithms=["HS256"]
+        )
+
+        return payload
+
+    except Exception:
+
+        raise HTTPException(
+            status_code=401,
+            detail="Invalid token"
+        )
+def require_role(*required_role: str):
+
+    def role_checker(
+        user = Depends(get_current_user)
+    ):
+
+        if user["role"] not in required_role:
+
+            raise HTTPException(
+                status_code=403,
+                detail="Permission denied"
+            )
+
+        return user
+
+    return role_checker
